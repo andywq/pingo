21461655d183458f32e145693a6e88ff6a4fba23
Alex (Jinghao) Yan <yanhao1991@gmail.com>
2020-11-02 13:48:20 +0800

    add morgan logs

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from "./app.module"
 import { ConfigService } from "./config/config.service"
 import { Logger } from "@nestjs/common"
 import { join } from "path"
+// import { default as morgan } from "morgan"
+const morgan = require("morgan")
 
 import * as bodyParser from "body-parser"
 import * as hbs from "hbs"
@@ -11,6 +13,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const { config, mode } = app.get(ConfigService)
 
+  app.use(morgan("short"))
   app.useStaticAssets(join(__dirname, "..", "public"))
   app.setBaseViewsDir(join(__dirname, "..", "views"))
   app.setViewEngine("hbs")
